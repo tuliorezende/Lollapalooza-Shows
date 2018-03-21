e5dfb858812639f5d70885c4779a5e81f67bae39
Túlio Rezende <tulio.mamute@hotmail.com>
2018-03-20 23:52:55 -0300

    adding cors permission

--- Lollapalooza.Api/Startup.cs
+++ Lollapalooza.Api/Startup.cs
@@ -34,6 +34,12 @@ namespace Lollapalooza.Api
         /// This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                       .AllowAnyMethod()
+                       .AllowAnyHeader();
+            }));
             services.AddMvc()
                  .AddJsonOptions(options =>
                  {
@@ -78,6 +84,7 @@ namespace Lollapalooza.Api
                 app.UseDeveloperExceptionPage();
 
             app.UseMiddleware<ErrorHandlingMiddleware>();
+            app.UseCors("MyPolicy");
             app.UseMvc();
         }
     }
